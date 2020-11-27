9a6840b6b4c22beb756a4365d4e9da5984fea522
Hiroaki Shimizu <hiroaki.shimizu@nikon.com>
2020-11-28 08:21:33 +0900

    9章まで

--- コンパイラ入門_山下/chapt9,10/sl1.sh
+++ コンパイラ入門_山下/chapt9,10/sl1.sh
@@ -5,6 +5,6 @@ gcc fig09_09.c fig09_20.c y.tab.c -o sl1
 
 cat fig09_06.SL1 | ./sl1
 
-
-cat fig09_02.SL0 | ./sl1
-cat fig09_02.SL0 | ./sl0
+cat fig09_02.SL0 | ./sl1 > res_a.txt 
+cat fig09_02.SL0 | ./sl0 > res_b.txt 
+diff res_a.txt res_b.txt
